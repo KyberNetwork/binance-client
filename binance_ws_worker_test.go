a87d75da6d52cc17031c2b8eed79d1eb64276e23
halink0803 <halink0803@gmail.com>
2020-09-22 04:57:27 +0000

    add test

---
+++ binance_ws_worker_test.go
@@ -0,0 +1,18 @@
+package binance
+
+import (
+	"testing"
+
+	"github.com/KyberNetwork/cex_account_data/common"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+)
+
+func TestParseAccountBalance(t *testing.T) {
+	data := []byte(`{"e":"outboundAccountPosition","E":1600749480712,"u":1600749480711,"B":[{"a":"BNB","f":"1.60268308","l":"0.00000000"},{"a":"USDT","f":"1000.24006746","l":"0.00000000"},{"a":"UNI","f":"1.54197400","l":"75.34000000"}]}`)
+	sugar := zap.L().Sugar()
+	accountDataWorker := NewAccountDataWorker(sugar, nil, nil, nil, "binance_1") // random account id
+	balance := []*common.PayloadBalance{}
+	err := accountDataWorker.parseAccountBalance(data, sugar, &balance)
+	require.NoError(t, err)
+}
